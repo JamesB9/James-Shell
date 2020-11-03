4efd9d6d64d7bcafd6d7a87760f0fc38cc920196
J-B <57415099+J-Burling@users.noreply.github.com>
2020-11-03 10:51:48 +0000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OS1_Coursework.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS1_Coursework.dir/main.c.obj"
+  "OS1_Coursework.exe"
+  "OS1_Coursework.exe.manifest"
+  "OS1_Coursework.lib"
+  "OS1_Coursework.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OS1_Coursework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
